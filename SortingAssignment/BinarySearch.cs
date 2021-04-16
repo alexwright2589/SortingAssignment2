9c2ed7ee5591cb6db2eb43066945792fe1323194
alexwright2589 <79853997+alexwright2589@users.noreply.github.com>
2021-04-16 22:13:58 +0100

    yes

--- SortingAssignment/BinarySearch.cs
+++ SortingAssignment/BinarySearch.cs
@@ -14,14 +14,14 @@ namespace SortingAssignment
             if (low > high)
             {
                 j++;
-                Console.WriteLine("This number is not in array" + j);
+                Console.WriteLine("This number is not in array " + j);
                 return -1;
             }
             int mid = (low + high) / 2;
             if (key == array[mid])
             {
                 j++;
-                Console.WriteLine("This number is in array" + j + "at position " + mid);
+                Console.WriteLine("This number is in array " + j + " at position " + mid);
                 return mid;
             }
             if (key < array[mid])
