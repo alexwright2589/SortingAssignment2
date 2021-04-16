c40640e49fda9b2a52d62b54eec422b6ef6df292
alexwright2589 <79853997+alexwright2589@users.noreply.github.com>
2021-04-16 17:35:51 +0100

    Add project files.

---
+++ SortingAssignment/BinarySearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAssignment
+{
+    public class BinarySearch
+    {
+        int j = 0;
+        public int BinarySearch_R(int key, int[] array, int low, int high)
+        {
+            if (low > high)
+            {
+                j++;
+                Console.WriteLine("This number is not in array" + j);
+                return -1;
+            }
+            int mid = (low + high) / 2;
+            if (key == array[mid])
+            {
+                j++;
+                Console.WriteLine("This number is in array" + j + "at position " + mid);
+                return mid;
+            }
+            if (key < array[mid])
+            {
+                return BinarySearch_R(key, array, low, mid - 1);
+            }
+            else
+            {
+                return BinarySearch_R(key, array, mid + 1, high);
+            }
+
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ SortingAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SortingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] Road1 = Array.ConvertAll(System.IO.File.ReadAllLines("Road_1_256.txt"), int.Parse);
+            int[] Road2 = Array.ConvertAll(System.IO.File.ReadAllLines("Road_2_256.txt"), int.Parse);
+            int[] Road3 = Array.ConvertAll(System.IO.File.ReadAllLines("Road_3_256.txt"), int.Parse);
+
+            Quicksort quickSort = new Quicksort();
+            int[] SortedRoad1 = quickSort.QuickSort(Road1);
+            int[] SortedRoad2 = quickSort.QuickSort(Road2);
+            int[] SortedRoad3 = quickSort.QuickSort(Road3);
+            GetEveryNthValue(SortedRoad1);
+            GetEveryNthValue(SortedRoad2);
+            GetEveryNthValue(SortedRoad3);
+
+            Console.WriteLine("What number would you like to search for?");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            BinarySearch binarySearch = new BinarySearch();
+            int NumberSearch1 = binarySearch.BinarySearch_R(Number, SortedRoad1, 0, SortedRoad1.Length - 1);
+            int NumberSearch2 = binarySearch.BinarySearch_R(Number, SortedRoad2, 0, SortedRoad2.Length - 1);
+            int NumberSearch3 = binarySearch.BinarySearch_R(Number, SortedRoad3, 0, SortedRoad3.Length - 1);
+
+
+        }
+        private static void GetEveryNthValue(int[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                if ((i == 0) || (i % 9 == 0))
+                {
+                    Console.WriteLine(data[i]);
+                }
+            }
+        }
+        
+    }
+}
